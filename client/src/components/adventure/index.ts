0ecfcc4b37c1d70380698b1afff809076c50f383
Tal Peled <talp2002@cs.colman.ac.il>
2026-06-09 09:52:42 +0300

    feat: add adventure component barrel export and image directories

---
+++ client/src/components/adventure/index.ts
@@ -0,0 +1,5 @@
+export { AdventureCard } from './AdventureCard';
+export type { AdventureCardProps } from './AdventureCard';
+export { TopicCard } from './TopicCard';
+export { WorldCard } from './WorldCard';
+export { StepIndicator } from './StepIndicator';
